3abe53ef9c5e32478b60c7e2000e2db48becb0f1
Sm Shahniaz <ss359@itl095.student.eecs.qmul.ac.uk>
2016-10-03 14:25:23 +0100

    Added

--- Unit1/initials.java
+++ Unit1/initials.java
@@ -1,30 +1,51 @@
-import javax.swing.*;
-/* Author: Sm Shahniaz
-   This is a program that prints initials using many variables.
+/* AUTHOR Sm Shahniaz 3 October 2016
+This is a program for concentrating strings. And to print out the initias of my name.
 */
 
-class initials 
-{
-	public static void main(String[] args) 
-	{
-		firstI();
-		lastI();
-		System.exit(0);
-	}
-	
-	
-	public static void main(String[] args) 
-	{
-		firstI();
-		lastI();
-		System.exit(0);
-	}
-	
-	public static void main(String[] args) 
-	{
-		firstI();
-		lastI();
-		System.exit(0);
-	}
+import javax.swing.*;
 
-}
+class initials
+{
+    public static void main (String[] param)
+    {
+        firsti(); //First Initial 
+	secondi(); //Second Initial
+        System.exit(0);
+
+    } // END main
+
+
+
+   /* ***************************************************
+	The initials split into two seperate methods.
+   */
+   
+   
+   // This is the method for the first initial.
+    public static void firsti ()
+    {
+       
+        String letter1;
+       
+        letter1 = "SSSSSS" + "\n" + "SS" + "\n" + "SS" + "\n" + "SSSSSS" + "\n" + "    SS" + "\n" + "    SS" + "\n" +"SSSSSS";
+        
+        System.out.println(letter1 + "\n"); // Should Print "S"
+
+        return;
+     } // END firsti
+    
+   // This is the method for the second initial.
+    public static void secondi()
+    {
+    
+    	String letter2;
+    	
+    	letter2 = "SSSSSS" + "\n" + "SS" + "\n" + "SS" + "\n" + "SSSSSS" + "\n" + "    SS" + "\n" + "    SS" + "\n" +"SSSSSS";
+    	
+    	System.out.println(letter2); //Should print "S"
+    	
+    	return;
+    
+    
+    } //END secondi		
+} // END class initials
