db09a9b5e3670b9f658dc9d6b10f1c2aa9611a1d
sChneizelJ <schneizel.jaeger@gmail.com>
2016-10-10 20:55:42 +0100

    Mini Project 2

--- miniprojects/musicquiz.java
+++ miniprojects/musicquiz.java
@@ -4,8 +4,8 @@
  * */
 
 
-import java.util.*;
-import javax.swing.*;
+import java.util.*;// Using Random and Scanner
+import javax.swing.*;// Here for virtually no reason
 
 
 class musicquiz
@@ -13,19 +13,19 @@ class musicquiz
 
   public static void main(String[] args)
   {
-    askQuenstions();
+    askQuestions();
     System.exit(0);
 
-  }
+  }//END main
 
   /*This method askQuestions contains all the questions for the quiz.
   and contains all the answers on the side comments*/
 
 
-  public static void askQuenstions()
+  public static void askQuestions()
   {
-    String openMesasge, q1, q2, q3, q4, q5, ans;
-    openMesasge = "Welcome to the Random Music Quiz!" + "\n" + "Let's Begin!";
+    String openMesasge; String q1; String q2; String q3; String q4; String q5; String ans;
+    openMesasge = "Welcome to the Random Music Quiz!" + "\n" + "Let's Begin!" + "\n";
     q1 = "Which of the following rock songs won the Best Rock Performance at the 2016 Grammy Awards?"; //Don't Wanna Fight
     q2 = "Justin Bieber had three of the top five songs on the Billboard Hot 100 chart for January 2nd, 2016. Sorry, Love Yourself and...?";//What Do You Mean?
     q3 = "What did Mike Posner take in Ibiza to show Avicii he was cool?"; //Pill
@@ -60,7 +60,7 @@ class musicquiz
     finalMessage();
 
     return;
-  }
+  }// END askQuestions
 
   /*This is the method for calling the user inputs thorugh the terminal
   returns the */
@@ -72,6 +72,21 @@ class musicquiz
 
     System.out.println(message);
 
+    System.out.println("Your score is " + score());
+
     return message;
+  }
+
+  /*This is a method for the random dice roll returning
+  the value and giving the score.*/
+
+  public static int score()
+  {
+    Random dice = new Random();
+
+    int diceThrow = dice.nextInt(6) + 1; //Generates random number between 1-6. Zero included hence +1
+
+    return diceThrow;
+
   }
 }
