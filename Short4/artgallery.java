331e47904447b0b3c43901af39136135fdb75931
sChneizelJ <schneizel.jaeger@gmail.com>
2016-10-11 15:02:05 +0100

    New updates
    Go home and figure out the extra bits and pieces of certain things about the records!!! :P

--- Short4/artgallery.java
+++ Short4/artgallery.java
@@ -13,14 +13,46 @@ class artgallery
         askvisitor();
         System.exit(0);
     }
+    /*Asks the user for the room they are in and hence then calling information from records
+    associated with that room*/
 
-    public static void askvisitor();
+    public static void askvisitor()
+    {
+        String userin;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which room are you in?");
+        userin = scanner.nextLine();
+        
+        if (foo)
+        {
+            System.out.println(" Something +s+ somethinf +s+ ");    
+        }
+        else if
+        {
+            System.out.println(" Something +s+ somethinf +s+ "); 
+        }
+        else if
+        {
+            System.out.println(" Something +s+ somethinf +s+ "); 
+        }
+        else if 
+        {
+            System.out.println(" Something +s+ somethinf +s+ "); 
+        }
+        else 
+        {
+            System.out.println(" Something +s+ somethinf +s+ "); //:P
+        }
+    
+    }// END askvisitor
 
-}
+}// END class artgallery
+
+/*This is teh class for storing the records of the different paintings and different info about them*/
 
 class artrecords
 {
     String direction; String artist; String title;
     int year;
     double height; double width;
-}
+}// END class artrecords
